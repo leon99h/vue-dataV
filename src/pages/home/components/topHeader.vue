fe8d4ecb88ab0fb3aeae554e0407e194dd4bc29b
M <m@Ms-MacBook-Pro.local>
2022-10-30 15:37:58 +0800

    add tit

--- src/pages/home/components/topHeader.vue
+++ src/pages/home/components/topHeader.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="top-header">
     <div class="times-box">
-      {{ nowDate }}
-      <img :src="img4" alt="" />
+      <span>{{ nowDate }}</span>
     </div>
-    <div class="c-t">
-      <img class="" :src="img2" alt="" />
-      <span class="text">系统总览</span>
+    <div class="c-t b1 t0">
+      系统总览
+    </div>
+    <div class="c-t b2 t1">
+      储能监视
+    </div>
+    <div class="c-t b2 t2">
+      储能控制
     </div>
     <div class="center-title">
       <p class="center-title-text">
         储能系统监控平台
       </p>
     </div>
-    <div class="esc" @click="onscreenfull"></div>
+    <div class="c-t b2 fz" style="right:400px">
+      <span>空调消防</span>
+    </div>
+    <div class="c-t b2 fz" style="right:274px">
+      <span>报警信息</span>
+    </div>
+    <div class="c-t b2 fz" style="right:147px">
+      <span>报表分析</span>
+    </div>
+    <div class="esc" @click="onscreenfull">
+      <span>ESC</span>
+    </div>
   </div>
 </template>
 
@@ -58,55 +73,65 @@ export default {
   height: 56px;
   box-shadow: 0px 0px 20px 6px rgba(67, 147, 170, 0.6) inset;
   .times-box {
-    width: 196px;
-    height: 48px;
-    border: 1px solid yellow;
+    width: 294px;
+    height: 72px;
     color: #a3cade;
-    font-size: 16px;
+    font-size: 24px;
     font-weight: 600;
-    line-height: 48px;
-    text-align: center;
-    margin: 20px 0 0 14px;
-    position: relative;
-    img {
-      width: 220px;
-      height: 48px;
-      border: 1px solid yellow;
-      position: absolute;
-      left: 0;
-      top: 0;
+    line-height: 72px;
+    background: url("./img/4.png") -12px;
+    background-size: 100% 100%;
+    transform: scale(0.5);
+    position: absolute;
+    top: -8px;
+    left: -56px;
+    span {
+      margin-left: 10px;
     }
   }
   // 文字
   .c-t {
-    width: 200px;
-    height: 50px;
-    border: 1px solid #000;
+    width: 138px;
+    height: 36px;
     position: absolute;
-    top: 20px;
-    left: 30px;
-    img {
-      position: absolute;
-      width: 100%;
-      z-index: 1;
-      &.f {
-        transform: rotateY(180deg);
-      }
+    top: 10px;
+    color: rgba(163, 202, 222, 1);
+    font-size: 15px;
+    line-height: 36px;
+    text-align: center;
+    z-index: 10;
+    &.b1 {
+      background: url("./img/2.png") no-repeat;
+      background-size: 100% auto;
     }
-    .text {
-      position: absolute;
-      z-index: 2;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      color: red;
+    &.t0 {
+      left: 147px;
+    }
+    &.t1 {
+      left: 274px;
+    }
+    &.t2 {
+      left: 402px;
+    }
+    &.b2 {
+      background: url("./img/3.png") no-repeat;
+      background-size: 100% 100%;
+    }
+    &.fz {
+      transform: rotateY(180deg);
+      span {
+        position: absolute;
+        transform: rotateY(180deg);
+        width: 100%;
+        top: 0;
+        left: 0;
+      }
     }
   }
   // 文字end
   .center-title {
     width: 388px;
     height: 52px;
-    border: 1px solid #000;
     position: absolute;
     font-size: 24px;
     font-weight: bold;
@@ -120,22 +145,25 @@ export default {
     .center-title-text {
       font-size: 24px;
       font-weight: 600;
-      color: #eff0f1;
       background: linear-gradient(0deg, #e0f2f6 0%, #8db8dc 100%);
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
     }
   }
   .esc {
-    width: 196px;
-    height: 48px;
+    width: 146px;
+    height: 36px;
+    line-height: 36px;
     background: url("./img/6.png");
     background-size: 100% 100%;
     position: absolute;
-    top: 20px;
-    right: 504px;
-    background: red;
-    z-index: 999;
+    top: 10px;
+    right: 10px;
+    span {
+      font-size: 15px;
+      color: #a3cade;
+      margin-left: 50px;
+    }
   }
 }
 </style>
